5ab810ab307a3abe2ba5e629a6d43423de53d0d8
EsbenHygum <esbenhl1@gmail.com>
2020-06-21 17:30:20 +0200

    finalized exam

--- exam/main.py
+++ exam/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 from math import *
 from montecarlo import *
 
@@ -8,6 +9,7 @@ def sphere(x): return x[0]**2*sin(x[1])
 
 a = [0, 0, 0]
 b = [1, pi, 2*pi]
+exact = 4/3*pi
 
 print("Testing the pseudo random Monte Carlo method")
 print("\nVolume of sphere with radius = 1:")
@@ -15,20 +17,21 @@ result, err = montecarlo_sampler(sphere, a, b, 15000)
 print("Result should be 4/3 pi (approximately 4.19)")
 print("Result is", result, "p/m", err)
 
-print("\nTesting the quasi random Monte Carlo method")
-d = len(a)
-print("Volume of sphere with radius = 1:")
-result, err = quasi_mc(sphere, a, b, 15000, d)
-print("Result should be 4/3 pi (approximately 4.19)")
-print("Result is", result, "p/m", err)
+#print("\nTesting the quasi random Monte Carlo method")
+#d = len(a)
+#print("Volume of sphere with radius = 1:")
+#result, err = quasi_mc(sphere, a, b, 15000, d, exact)
+#print("Result should be 4/3 pi (approximately 4.19)")
+#print("Result is", result, "p/m", err)
 
-print("\n\nTesting convergence on error of sphere:\nSee plot\nThe expected result has been multiplied by a factor of 5")
+
+print("\n\nTesting convergence on error of sphere:\nSee plot")
 
 error = []
 convergence = []
 iteration = []
 
-for ii in range(10, 1000, 10): 
+for ii in range(10, 10000, 10): 
     result, err = montecarlo_sampler(sphere, a, b, ii)
     error.append(err)
     convergence.append(5/sqrt(ii))
@@ -38,11 +41,19 @@ q_error = []
 q_convergence = []
 d = len(a)
 
-for ii in range(10, 1000, 10): 
-    q_result, q_err = quasi_mc(sphere, a, b, ii, d)
-    q_error.append(q_err)
-    q_convergence.append(log(ii)/(ii))
 
+for ii in range(10, 10000, 10): 
+	seq = halton(2, ii)
+	accumu = 0
+	for jj in range(ii):
+		x = 1 + seq[jj][0]*(5 - 1)
+		y = 1 + seq[jj][1]*(5**2 - 1**2)
+		accumu += x**2
+	volume = 5 - 1
+	result = volume*accumu/ii
+	q_error.append(np.abs(result - 41.333333))
+	q_convergence.append(22*log(ii)/(ii))
+#	q_result, q_err = quasi_mc(sphere, a, b, ii, d, exact)
 
 np.savetxt('convergence.txt', list(zip(iteration, error, convergence, q_error, q_convergence)))
 
--- exam/montecarlo.py
+++ exam/montecarlo.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 from math import *
 
 def montecarlo_sampler(f, a, b, N):
@@ -29,35 +30,49 @@ def corput(N, base):
 		bk /= base
 	return q
 
-def halton(N, d):
-	base = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67]
-	maxd = len(base)
-	assert d <= maxd
-	x = []
-	for ii in range(0, d):
-		x.append(corput(N, base[ii]))
-	return x
+#def halton(N, d):
+#	base = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67]
+#	maxd = len(base)
+#	assert d <= maxd
+#	x = []
+#	for ii in range(0, d):
+#		x.append(corput(N, base[ii]))
+#	return x
 
-def quasi_random_xsampler(a, b, N, d):
-	x_halton = halton(N, d)
-	return [a[jj] + x_halton[jj]*(b[jj] - a[jj]) for jj in range(len(a))]
-
-def quasi_mc(f, a, b, N, d):
+def quasi_mc(f, a, b, N, d, exact):
 	V = 1.0
 	for jj in range(len(a)):
 		V *= b[jj] - a[jj]
 	q_montecarlo_sum = 0.0
-	q_montecarlo_sum2 = 0.0
-	montecarlo_sum = 0.0
-	montecarlo_sum2 = 0.0
 	for jj in range(0, N):
-		fxq = f(quasi_random_xsampler(a, b, N, d))
+		fxq = f(halton(jj, d))
 		q_montecarlo_sum += fxq
-		q_montecarlo_sum2 += fxq*fxq
-		fx = f(x_sampler(a, b))
-		montecarlo_sum += fx
-		montecarlo_sum2 += fx*fx
-	error = np.abs(V*(montecarlo_sum/N) - V*(q_montecarlo_sum/N))
+	error = np.absolute(V*(q_montecarlo_sum/N) - exact)
+	print(V*(q_montecarlo_sum/N))
 	return V*(q_montecarlo_sum/N), error
+
+def halton(dim: int, num: int):
+	h = np.full(num*dim, np.nan)
+	p = np.full(num, np.nan)
+	P = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
+	logn = math.log(num + 1)
+	for ii in range(dim):
+		b = P[ii]
+		n = int(math.ceil(logn / math.log(b)))
+		for t in range(n):
+			p[t] = pow(b, -(t+1))
+		for jj in range(num):
+			d = jj + 1
+			sum_ = math.fmod(d, b) * p[0]
+			for t in range(1, n):
+				d = math.floor(d / b)
+				sum_ += math.fmod(d, b) * p[t]
+			h[jj * dim + ii] = sum_
+	return h.reshape(num, dim)
+
+
+
+
+
 
 
